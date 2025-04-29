e22ac287675182ba68f06ab6bf59d6449c2dac07
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-29 16:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	const content = document.createElement(`p`)
+	content.innerText = 'DOM load success'
+	document.body.appendChild(content)
+});
